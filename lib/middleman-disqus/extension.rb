a285b4eb97f532143881f9c8956eabbc61816c81
Luke Antins <luke@lividpenguin.com>
2014-08-06 04:14:18 +0100

    Place html/javascript into .erb templates

--- lib/middleman-disqus/extension.rb
+++ lib/middleman-disqus/extension.rb
@@ -6,45 +6,30 @@ module Middleman
 
     def initialize(app, options_hash={}, &block)
       super
-      app.set :disqus_shortname, options.shortname
+      # place in class variable so helpers can access
+      @@options = options
+    end
+
+    def self.options(options = {})
+      @@options.to_h.merge(options)
     end
 
     helpers do
       def disqus
-        return '' unless disqus_shortname
-        %Q{
-          <div id="disqus_thread"></div>
-          <script type="text/javascript">
-          //<![CDATA[
-              var disqus_shortname = '#{disqus_shortname}';
-              (function() {
-                  var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-                  dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-                  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-              })();
-          //]]>
-          </script>
-          <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-          <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
-        }
+        @options = Middleman::DisqusExtension.options({})
+        return '' unless @options[:shortname]
+
+        file = File.join(File.dirname(__FILE__), 'embed.erb')
+        ERB.new(File.read(file)).result(binding)
       end
 
       def disqus_count
-        return '' unless disqus_shortname
-        %Q{
-          <script type="text/javascript">
-          //<![CDATA[
-              var disqus_shortname = '#{disqus_shortname}';
+        @options = Middleman::DisqusExtension.options({})
+        return '' unless @options[:shortname]
+        #current_resource.metadata[:page].merge(disqus_settings)
 
-              (function () {
-                  var s = document.createElement('script'); s.async = true;
-                  s.type = 'text/javascript';
-                  s.src = '//' + disqus_shortname + '.disqus.com/count.js';
-                  (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-              }());
-          //]]>
-          </script>
-        }      
+        file = File.join(File.dirname(__FILE__), 'count.erb')
+        ERB.new(File.read(file)).result(binding)
       end
     end
 
