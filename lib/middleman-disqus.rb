0c9452dc1ad5ac01fde12d5b182f6dc893c8ce59
Luke Antins <luke@lividpenguin.com>
2014-08-06 02:06:37 +0100

    Remove one level of indentation

--- lib/middleman-disqus.rb
+++ lib/middleman-disqus.rb
@@ -11,25 +11,25 @@ class Disqus < ::Middleman::Extension
   
   helpers do
     def disqus
-      if disqus_shortname
-        %Q{
-          <div id="disqus_thread"></div>
-          <script type="text/javascript">
-          //<![CDATA[
-              var disqus_shortname = '#{disqus_shortname}';
-              (function() {
-                  var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-                  dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-                  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-              })();
-          //]]>
-          </script>
-          <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-          <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
-        }
-      end
+      return '' unless disqus_shortname
+      %Q{
+        <div id="disqus_thread"></div>
+        <script type="text/javascript">
+        //<![CDATA[
+            var disqus_shortname = '#{disqus_shortname}';
+            (function() {
+                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            })();
+        //]]>
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+        <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+      }
     end
   end
+
 end
 
 ::Middleman::Extensions.register(:disqus, Disqus)
