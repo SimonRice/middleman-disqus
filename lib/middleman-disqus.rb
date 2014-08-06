f6de9da586aa589055d2a89296aefd3ca15b1b9a
Luke Antins <luke@lividpenguin.com>
2014-08-06 03:18:05 +0100

    Jiggled project directory structure and adjusted how extension is registered to line up with official middleman extensions

--- lib/middleman-disqus.rb
+++ lib/middleman-disqus.rb
@@ -1,53 +1,7 @@
-require "middleman-core"
+require 'middleman-core'
+require 'middleman-disqus/version'
 
-class Disqus < ::Middleman::Extension
-  option :shortname, false, "Your short name for Disqus"
-  
-  def initialize(app, options_hash={}, &block)
-    super
-    
-    app.set :disqus_shortname, options.shortname
-  end
-  
-  helpers do
-    def disqus
-      return '' unless disqus_shortname
-      %Q{
-        <div id="disqus_thread"></div>
-        <script type="text/javascript">
-        //<![CDATA[
-            var disqus_shortname = '#{disqus_shortname}';
-            (function() {
-                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-                dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-            })();
-        //]]>
-        </script>
-        <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-        <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
-      }
-    end
-
-    def disqus_count
-      return '' unless disqus_shortname
-      %Q{
-        <script type="text/javascript">
-        //<![CDATA[
-            var disqus_shortname = '#{disqus_shortname}';
-
-            (function () {
-                var s = document.createElement('script'); s.async = true;
-                s.type = 'text/javascript';
-                s.src = '//' + disqus_shortname + '.disqus.com/count.js';
-                (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-            }());
-        //]]>
-        </script>
-      }      
-    end
-  end
-
+::Middleman::Extensions.register(:disqus) do
+  require 'middleman-disqus/extension'
+  ::Middleman::DisqusExtension
 end
-
-::Middleman::Extensions.register(:disqus, Disqus)
